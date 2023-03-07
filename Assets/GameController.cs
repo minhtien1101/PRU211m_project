f5aac0245d6e09cf5833dbb683e42279bb74e639
PhiMinhTien <pmt11112001@gmail.com>
2023-03-07 21:23:04 +0700

    add menu game

---
+++ Assets/GameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	[SerializeField] GameObject menuPanel;
+	[SerializeField] GameObject tutorialPanel;
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Scene1");
+	}
+
+	public void ViewTutorial()
+	{
+		menuPanel.SetActive(false);
+		tutorialPanel.SetActive(true);
+	}
+	public void CloseTutorial()
+	{
+		menuPanel.SetActive(true);
+		tutorialPanel.SetActive(false);
+	}
+}
