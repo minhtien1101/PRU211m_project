7ec428e278713672e4721a60ba2d8c1f65dfaeb7
PhiMinhTien <pmt11112001@gmail.com>
2023-03-11 08:52:43 +0700

    fix parallax effect

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -5,28 +5,65 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-
-	[SerializeField] float parallaxEffect = 0.5f;
 	Transform cameraTransform;
-	Vector3 lastCameraPiont;
-	// Start is called before the first frame update
-	void Start()
+	[SerializeField] float currentDis;
+	[SerializeField] float limitStartDis;
+	[SerializeField] float limitEndDis;
+	[SerializeField] float respawnDis;
+
+	public void Start()
 	{
 		cameraTransform = Camera.main.transform;
-		lastCameraPiont = cameraTransform.position;
 	}
-
-	// Update is called once per frame
-	void LateUpdate()
+	public void FixedUpdate()
 	{
-		Vector3 deltaMovement = cameraTransform.position - lastCameraPiont;
-		transform.position += deltaMovement * parallaxEffect;
-		lastCameraPiont = cameraTransform.position;
+		GetDistance();
+		Spawning();
 
-		if (Math.Abs(cameraTransform.position.x - transform.position.x) >= 12.8f)
-		{
-			float offsetPositionX = (cameraTransform.position.x - transform.position.x) % 12.8f;
-			transform.position += new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y, transform.position.z);
-		}
 	}
+
+	private void Spawning()
+	{
+		if (limitStartDis < currentDis && currentDis < limitEndDis) return;
+		Vector3 pos = transform.position;
+		if (currentDis >= limitEndDis)
+		{
+			pos.x += respawnDis;
+		}
+		else if (currentDis <= limitStartDis)
+		{
+			pos.x += -1 * respawnDis;
+		}
+
+		transform.position = pos;
+	}
+
+	private void GetDistance()
+	{
+		currentDis = cameraTransform.transform.position.x - transform.localPosition.x;
+		Debug.Log(currentDis);
+	}
+	//[SerializeField] float parallaxEffect = 0.5f;
+	//Transform cameraTransform;
+	//Vector3 lastCameraPiont;
+	//// Start is called before the first frame update
+	//void Start()
+	//{
+	//	cameraTransform = Camera.main.transform;
+	//	lastCameraPiont = cameraTransform.position;
+	//}
+
+	//// Update is called once per frame
+	//void LateUpdate()
+	//{
+	//	Vector3 deltaMovement = cameraTransform.position - lastCameraPiont;
+	//	transform.position += deltaMovement * parallaxEffect;
+	//	lastCameraPiont = cameraTransform.position;
+
+	//	if (Math.Abs(cameraTransform.position.x - transform.position.x) >= 12.8f)
+	//	{
+	//		float offsetPositionX = (cameraTransform.position.x - transform.position.x) % 12.8f;
+	//		transform.position += new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y, transform.position.z);
+	//	}
+	//}
 }
